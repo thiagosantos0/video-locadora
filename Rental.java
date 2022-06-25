32d72fe9ab312756ee7ba2d97cc6fbc2f4903879
Thiago Henrique <thiagosantosgg0@gmail.com>
2022-06-25 19:05:22 -0300

    Commit 10
    Commit 10

--- Rental.java
+++ Rental.java
@@ -21,13 +21,7 @@ public class Rental {
     }
 
     public int getFrequentRenterPoints(){
-
-        // add bonus for a two day new release rental
-        if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                this.getDaysRented() > 1)
-            return 2;
-
-        return 1;
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
 
 
