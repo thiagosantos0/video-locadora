e00a625118e15d63adbfba87d5486366351ddfad
Thiago Henrique <thiagosantosgg0@gmail.com>
2022-06-25 19:19:58 -0300

    Commit 15
    Commit 15

--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -7,4 +7,7 @@ public class NewReleasePrice extends Price {
         return daysRented * 3;
     }
 
+    public int getFrequentRenterPoints(int daysRented) {
+        return (daysRented > 1) ? 2: 1;
+    }
 }
